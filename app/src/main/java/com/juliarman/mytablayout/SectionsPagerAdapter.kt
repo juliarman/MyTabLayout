17a95d01a935fc89d9d9af592d723b1ea1adc563
Juliarman Umar <juliarmanbone@gmail.com>
2020-08-02 03:19:31 +0800

    TabLayout Android

---
+++ app/src/main/java/com/juliarman/mytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,45 @@
+package com.juliarman.mytablayout
+
+import android.content.Context
+import androidx.annotation.Nullable
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class SectionsPagerAdapter (private val mContext: Context, fm: FragmentManager): FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+
+    @StringRes
+
+    private val TAB_TITLES = intArrayOf(R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3)
+    override fun getItem(position: Int): Fragment {
+
+
+        val fragment = HomeFragment.newInstance(position +1)
+
+
+      /*  var fragment: Fragment?= null
+        when(position){
+             0 -> fragment = HomeFragment()
+             1 -> fragment = ProfilFragment()
+             2 -> fragment = SettingFragment()
+        }
+      */
+
+
+        return fragment as Fragment
+
+    }
+
+    @Nullable
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+
+    }
+
+    override fun getCount(): Int {
+
+        return 3
+
+    }
+}
